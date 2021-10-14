c2cc0ae9e5fe2d3d3241730b419e296e021e37a2
Fadli Ramadhan <fadlir93@gmail.com>
2021-10-14 11:11:09 +0800

    component search

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,19 +1,74 @@
 import React from 'react';
 import {
     View,
-    Text
+    Text,
+    TouchableOpacity,
+    Image,
+    TextInput,
+    FlatList
 } from 'react-native';
+import { FONTS, SIZES, COLORS, icons, dummyData} from '../../constants'
 
 const Home = () => {
+
+    const randerSearch = () => {
+        return (
+            <View
+            style={{
+                flexDirection: 'row',
+                height: 40,
+                alignItems: 'center',
+                marginHorizontal: SIZES.padding,
+                marginVertical: SIZES.base,
+                paddingHorizontal: SIZES.radius,
+                borderRadius: SIZES.radius,
+                backgroundColor: COLORS.lightGray2
+            }}>
+            {/* icon */}
+                <Image
+                    source={icons.search}
+                    style={{
+                        height: 20,
+                        width: 20,
+                        tintColor: COLORS.black
+                    }}
+                />
+                {/* Text input */}
+                <TextInput 
+                    style={{
+                        flex: 1,
+                        marginLeft: SIZES.radius,
+                        ...FONTS.body3
+                    }}
+                    placeholder="search"
+                />
+
+                {/* filter */}
+                <TouchableOpacity>
+                    <Image
+                        source={icons.filter}
+                        style={{
+                            height: 20,
+                            width: 20,
+                            tintColor: COLORS.black
+                        }}
+                    />
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
     return (
         <View
             style={{
                 flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center'
             }}
         >
-            <Text>Home</Text>
+            {/* Search */}
+                {randerSearch()}
+
+            {/* List */}
+
         </View>
     )
 }
